0aedddf72d8f11f15c5ba70ed1cd82a727af300e
Diki Rahman <12250113908@students.uin-suska.ac.id>
2022-12-14 09:59:25 +0700

    git init

--- src/com/kellima/pemdal/MEOnlineStore.java
+++ src/com/kellima/pemdal/MEOnlineStore.java
@@ -1,5 +1,6 @@
 package com.kellima.pemdal;
 
+//PERSISTENCE LEBIH GAMPANG KALO PAKE CSV Writer dan CSV READER
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
@@ -7,9 +8,10 @@ import java.awt.event.ActionListener;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Objects;
 
-public class MEOnlineStore {
+public class MEOnlineStore{
     private JTextField textbrandproduk;
     private JTextField textKustomer;
     private JTextField textharga;
@@ -18,11 +20,10 @@ public class MEOnlineStore {
     private JTable tabelData;
     private JComboBox comboboxjenisproduk;
     private JPanel rootPanel;
+    private JButton reset;
     private final DataMEOstore objMEO;
     private String totalHarga;
-
     private DefaultTableModel tableModel;
-
     public MEOnlineStore() {
 
         this.objMEO = new DataMEOstore();
@@ -53,7 +54,7 @@ public class MEOnlineStore {
                 String dataInput = "\nKustomer :" + getkustomer + "\nMerek Produk : " + getmerekproduk + "\nJenis Produk : " + getjenisproduk + "\nHarga Produk : " + getharga + "\nKuantitas Pesanan : " + getjumlahbarang + "\nTotal Harga : " + totalHarga + "\n\n";
                 String Temp = objMEO.getKustomer() + "#" + objMEO.getMerek() + "#" + objMEO.getJenisproduk() + "#" + objMEO.getHarga() + "#" + objMEO.getJumlah() + "#" + objMEO.hargaAkhir() + "\n";
 
-                //Write data ke File StoredTextDatabase.txt dan TempStrArr
+                //Write data ke File StoredTextDatabase.txt dan TempStrArr.txt
                 FileWriter fw;
                 try {
                     fw = new FileWriter("StoredTextDatabase.txt", true);
@@ -73,6 +74,43 @@ public class MEOnlineStore {
             }
 
         });
+        reset.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try{
+
+                    FileWriter fw = new FileWriter("StoredTextDatabase.txt", false);
+                    PrintWriter pw = new PrintWriter(fw, false);
+                    pw.flush();
+                    pw.close();
+                    fw.close();
+                }catch(Exception exception) {
+                    System.out.println("Exception have been caught");
+                }
+                try{
+                    FileWriter fw = new FileWriter(System.getProperty("user.dir") + "/src/TEMPStrArr.txt", false);
+                    PrintWriter pw = new PrintWriter(fw, false);
+                    pw.flush();
+                    pw.close();
+                    fw.close();
+                }catch(Exception exception) {
+                    System.out.println("Exception have been caught");
+                }
+                Object[] tableColumn = {"Kustomer",
+                        "Merek Produk",
+                        "Jenis Produk",
+                        "Harga",
+                        "Kuantitas Pesanan",
+                        "Total Harga"};
+                try {
+                    tableModel = new DefaultTableModel(new DataMEOstore().getmObject(), tableColumn);
+                } catch (FileNotFoundException a) {
+                    throw new RuntimeException(a);
+                }
+                tabelData.setModel(tableModel);
+                tabelData.setAutoCreateRowSorter(true);
+            }
+        });
     }
 
     //Initializer Komponen Tabel
@@ -84,20 +122,17 @@ public class MEOnlineStore {
                 "Harga",
                 "Kuantitas Pesanan",
                 "Total Harga"};
-
-        /*Munculkan tabel dan data di TempStrArr ke tabel
+        /*Membuat tabel dan munculkan data di TempStrArr.txt ke tabel
         Serta membuat data tetap di tabel walau aplikasi telah di close
          */
         try {
             tableModel = new DefaultTableModel(new DataMEOstore().getmObject(), tableColumn);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+        } catch (FileNotFoundException a) {
+            throw new RuntimeException(a);
         }
         tabelData.setModel(tableModel);
         tabelData.setAutoCreateRowSorter(true);
-
     }
-
     //RootPanel
     public JPanel getRoot() {
         return rootPanel;
